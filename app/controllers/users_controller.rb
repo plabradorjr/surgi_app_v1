a8e13d9d87438839f951a2dec7363b98e1fc40ca
plabradorjr <plabradorjr@gmail.com>
2020-02-15 14:26:27 -0600

    email params taken out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,23 +10,23 @@ class UsersController < ApplicationController
 
   post '/signup' do
     downcase_name = params[:username].downcase
-    downcase_email = params[:email].downcase
+    # downcase_email = params[:email].downcase
     username_taken = User.find_by(:username => downcase_name)
-    email_taken = User.find_by(:email => downcase_email)
+    # email_taken = User.find_by(:email => downcase_email)
     if username_taken
         @error_message = "Sorry, that username is already taken."
         erb :'/users/error_message'
-    elsif email_taken
-        @error_message = "Sorry, that email is already taken."
-        erb :'/users/error_message'
+    # elsif email_taken
+    #     @error_message = "Sorry, that email is already taken."
+    #     erb :'/users/error_message'
     elsif !valid_login?(params[:username])
         @error_message = "Sorry, special characters are not allowed in username. Choose only words, underscore and numbers. Please try again."
         erb :'/users/error_message'
-    elsif params[:username] == "" || params[:email] == "" || params[:password] == ""
+    elsif params[:username] == "" || params[:password] == ""
         @error_message = "Sorry, all fields must be filled out. Try again."
         erb :'/users/error_message'
     else
-      @user = User.new(:username => downcase_name, :email => downcase_email, :password => params[:password])
+      @user = User.new(:username => downcase_name, :password => params[:password])
       @user.save
       session[:user_id] = @user.id
       redirect to '/home'
